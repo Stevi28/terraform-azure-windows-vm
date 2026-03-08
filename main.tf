e9fc2bc310068434f3040eb62d3e5b0088fd48bc
Stevi28 <stav.kourou19@gmail.com>
2026-03-08 20:21:00 +0200

    add_storage_account

--- main.tf
+++ main.tf
@@ -114,3 +114,17 @@ resource "azurerm_subnet_network_security_group_association" "bastion_assoc" {
     network_security_group_id = azurerm_network_security_group.bastion_nsg.id
 }
 
+# 4. Storage account for terraform state
+resource "azurerm_storage_account" "terraform_state" {
+    name = "sttfstate2026"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "state_container" {
+    name = "tfstate"
+    storage_account_name = azurerm_storage_account.terraform_state.name
+    container_access_tier = "private"
+}
