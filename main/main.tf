460682947f459f560f0bac3643c7a6c714b90db8
Stevi28 <stav.kourou19@gmail.com>
2026-03-08 23:59:30 +0200

    add bastion to tf

--- main/main.tf
+++ main/main.tf
@@ -119,3 +119,25 @@ data "azurerm_storage_account" "terraform_state" {
   name                = "sttfstate2026weu28"
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+# 6. Azure Bastion
+# Public IP
+resource "azurerm_public_ip" "bastion_pip" {
+  name                = "pip-bastion"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+} 
+# Host
+resource "azurerm_bastion_host" "bastion" {
+  name                = "bastion-lab"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_pip.id
+  }
+}
